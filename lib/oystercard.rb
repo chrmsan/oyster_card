806e6a1f8cae8784daff706d48d8ddad001dbb04
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-04 01:00:45 +0200

    Commit added touch in, touch out and in_journey method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,34 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   DEF_MAX_VAL = 90
+  DEF_MIN_FARE = 1
 
     def initialize
       @balance = 0
+      @in_travel = false
     end 
 
     def top_up(value)
       fail "Added money surpasses Oystercard limit of #{DEF_MAX_VAL} GBP" if error_max(value)
       @balance += value
+    end
+
+    def deduct(fare)
+      @balance -= fare
+    end
+
+    def in_journey
+      @in_travel
+    end
+
+    def touch_in
+      @in_travel = true
+    end
+
+    def touch_out
+      @in_travel = false
     end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,26 +4,71 @@ require 'oystercard'
 describe Oystercard do
 
 
-  describe '#top_up' do
-  
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-    
-    it 'should be able to add money to the card fi. 30' do
-      expect{ subject.top_up(30) }.to change{ subject.balance }.by(30)
-    end 
-
-  end
-
-
 
   it "should have balance of 0 when initialized" do
     expect(subject.balance).to eq 0
   end
 
-  it "should raise an error message when value added to card surpasses #{described_class::DEF_MAX_VAL}" do
-    subject.top_up(described_class::DEF_MAX_VAL)
-    expect { subject.top_up(1) }.to raise_error "Added money surpasses Oystercard limit of #{described_class::DEF_MAX_VAL} GBP"
+
+
+  describe '#top_up' do
+  
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    
+    it 'should be able to add money to the card fi. 30' do
+      expect { subject.top_up(30) }.to change{ subject.balance }.by(30)
+    end 
+
+    it "should raise an error message when value added to card surpasses default maximum value" do
+      max_val = described_class::DEF_MAX_VAL
+      subject.top_up(max_val)
+      expect { subject.top_up(1) }.to raise_error "Added money surpasses Oystercard limit of #{max_val} GBP"
+    end
+
   end
+
+
+
+  describe '#deduct' do
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'should be able to deduct min fare price from card' do
+      min_fare = described_class::DEF_MIN_FARE
+      subject.top_up(described_class::DEF_MAX_VAL)
+      expect { subject.deduct(min_fare) }.to change { subject.balance }.by(-min_fare)
+    end
+
+  end
+
+
+
+  describe '#in_journey' do
+
+    it { is_expected.to respond_to(:in_journey) }
+    
+    it { is_expected.to respond_to(:touch_in) }
+
+    it { is_expected.to respond_to(:touch_out) }
+
+
+    it 'should default in_journey initialize with false and before touch_in check that card is not in use' do
+      expect { subject.in_journey.to be_false }
+    end
+
+    it 'should change in_travel from false to true when using touch_in' do
+      subject.touch_in
+      expect { subject.in_journey.to be_true }
+    end
+
+    it 'should change in_travel from true to false when using touch_out' do
+      subject.touch_in
+      subject.touch_out
+      expect { subject.in_journey.to be_false }
+    end
+  
+  end
+
 
 
 end

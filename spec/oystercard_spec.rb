c3661fabceeea9633a5c2aad6816e8edde3d8a88
Tomy8s <tomy8s@hotmail.com>
2016-10-03 14:37:31 +0100

    test for top-upability

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,5 +6,9 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end 
 
-
+  it "can be topped-up" do
+    value = rand(1000)
+    subject.top_up(value)
+    expect(subject.balance).to eq value
+  end
 end
